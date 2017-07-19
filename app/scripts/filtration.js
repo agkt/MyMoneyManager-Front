1abe7e75ab1bd82d77337fc6b458232da08a9abb
katemrrr <katrinb_08@mail.ru>
2017-07-19 13:13:18 +0300

    fix filtration

--- app/scripts/filtration.js
+++ app/scripts/filtration.js
@@ -1,12 +1,12 @@
 /**
  * Created by katemrrr on 18.07.17.
  */
+
 // при загрузке страницы загружается полный список
 $(window).load(function(){
     $('#select_category').hide();
     showData(data.operations);
 });
-
 
 // массив данных
 var data = {
@@ -16,113 +16,114 @@ var data = {
             way: 'Кошелёк',
             category: 'Расходы',
             price: -2230,
-            date: '18.07.2017'
+            date: new Date(2017, 6, 18)
         },
         {
             name: 'Путешествия',
             way: 'Рокетбанк',
             category: 'Расходы',
             price: -8320,
-            date: '18.07.2017'
+            date: new Date(2017, 6, 18)
         },
         {
             name: 'Основная работа',
             way: 'Рокетбанк',
             category: 'Доходы',
             price: +40000,
-            date: '16.07.2017'
+            date: new Date(2017, 6, 16)
         },
         {
             name: 'Рокетбанк',
             way: 'Кошелёк',
             category: 'Переводы',
             price: 20230,
-            date: '16.07.2017'
+            date: new Date(2017, 6, 16)
         },
         {
             name: 'Жуковец Никита',
             way: 'Сбербанк',
             category: 'Долги',
             price: -800,
-            date: '17.07.2017'
+            date: new Date(2017, 6, 17)
         },
         {
             name: 'Покупки',
             way: 'Сбербанк',
             category: 'Расходы',
             price: -2230,
-            date: '17.07.2017'
+            date: new Date(2017, 6, 17)
         },
         {
             name: 'Одежда',
             way: 'Рокетбанк',
             category: 'Расходы',
             price: -8320,
-            date: '16.07.2017'
+            date: new Date(2017, 6, 16)
         },
         {
             name: 'Подработка',
             way: 'Рокетбанк',
             category: 'Доходы',
             price: +12000,
-            date: '16.07.2017'
+            date: new Date(2017, 6, 16)
         },
         {
             name: 'Рокетбанк',
             way: 'Кошелёк',
             category: 'Переводы',
             price: 2000,
-            date: '16.07.2017'
+            date: new Date(2017, 6, 16)
         },
         {
             name: 'Пушков Александр',
             way: 'Сбербанк',
             category: 'Долги',
             price: -300,
-            date: '15.07.2017'
+            date: new Date(2017, 6, 15)
         },
         {
             name: 'Еда',
             way: 'Кошелёк',
             category: 'Расходы',
             price: 20,
-            date: '15.07.2017'
+            date: new Date(2017, 6, 15)
         },
         {
             name: 'Еда',
             way: 'Рокетбанк',
             category: 'Расходы',
             price: 8320,
-            date: '15.07.2017'
+            date: new Date(2017, 6, 15)
         },
         {
             name: 'Фриланс',
             way: 'Рокетбанк',
             category: 'Доходы',
             price: 4000,
-            date: '15.07.2017'
+            date: new Date(2017, 6, 15)
         },
         {
             name: 'Сбербанк',
             way: 'Рокетбанк',
             category: 'Переводы',
             price: 20230,
-            date: '15.07.2017'
+            date: new Date(2017, 6, 15)
         },
         {
             name: 'Никитин Никита',
             way: 'Сбербанк',
             category: 'Долги',
             price: -800,
-            date: '14.07.2017'
+            date: new Date(2017, 6, 19)
         }
-
     ]
 };
 
 function fillDataType() {
 
     $('.history-flex-item').remove();
+    $('.history-page h4').remove();
+    $('.history-flex-container').remove();
 
     var filterData = [];
     var waySel = $('#select_way option:selected').text(); // кошелек/карта
@@ -153,95 +154,98 @@ function fillDataType() {
             filterData.push(data.operations[j]);
         }
     }
-    // console.log(filterData);
+
     showData(filterData);
 }
-// function fillTable() {
-//     for (i = 0; i < data.operations.length; i++) {
-//         if (data.operations[i].category == '') {
-//             $('.table').append("<tr>" +
-//                 "<td><p> Операция:" + data.operations[i].name + "  Сумма:" + data.operations[i].price + "  Кошелек:"
-//                 + data.operations[i].way + "</p></td>" +
-//                 "</tr>"
-//             );
-//         } else {
-//             $('.table').append("<tr>" +
-//                 "<td><p> Операция:" + data.operations[i].name + "  Сумма:" + data.operations[i].price + "  Кошелек:"
-//                 + data.operations[i].way + "</p></td>" +
-//                 "</tr>"
-//             );
-//         }
-//     }
-// }
 
 function showData(arr) {
-    for (var  i = 0; i < arr.length; i++){
-        // console.log(data.operations[i].category);
-        if (arr[i].category == "Расходы") {
-            $('.history-flex-container').append("" +
+    sortDate(arr);
+    if(arr.length == 0) {
+        $('.history-page').append("<h4>Ничего не найдено</h4>");
+        return;
+        //если ничего не найдено
+    }
+
+    printDate(arr[0].date);
+    printElement(arr[0]);
+    for (var  i = 1; i < arr.length; i++){
+        if (arr[i-1].date - arr[i].date != 0) {
+            printDate(arr[i].date);
+        }
+        printElement(arr[i]);
+    }
+}
+
+function printElement(element) {
+    switch (element.category) {
+        case "Расходы":
+            $('.history-flex-container:last-child').append("" +
                 "<div class='history-flex-item costs' data-toggle='modal' data-target='#history-costs__modal-window'>" +
-                "<div>" +
-                "<strong>"+ arr[i].name +"</strong><br>" +
-                "<small>"+ arr[i].way + "</small> " +
-                "</div> " +
-                "<div> " +
-                "<small>"+ arr[i].category +"</small> " +
-                "<br> " +
-                "<strong>"+ arr[i].price +" &#8381;</strong>" +
-                "</div> " +
+                printContentElement(element) +
                 "</div>");
-        } else if ((arr[i].category == "Долги") && (arr[i].price < 0)) {
-            $('.history-flex-container').append("" +
-                "<div class='history-flex-item own-dept' data-toggle='modal' data-target='#history-own-dept__modal-window'>" +
-                "<div>" +
-                "<strong>"+ arr[i].name +"</strong><br>" +
-                "<small>"+ arr[i].way + "</small> " +
-                "</div> " +
-                "<div> " +
-                "<small>"+ arr[i].category +"</small> " +
-                "<br> " +
-                "<strong>"+ arr[i].price +" &#8381;</strong>" +
-                "</div> " +
-                "</div>");
-        } else if ((arr[i].category == "Долги") && (arr[i].price > 0)) {
-            $('.history-flex-container').append("" +
-                "<div class='history-flex-item dept' data-toggle='modal' data-target='#history-dept__modal-window'>" +
-                "<div>" +
-                "<strong>"+ arr[i].name +"</strong><br>" +
-                "<small>"+ arr[i].way + "</small> " +
-                "</div> " +
-                "<div> " +
-                "<small>"+ arr[i].category +"</small> " +
-                "<br> " +
-                "<strong>"+ arr[i].price +" &#8381;</strong>" +
-                "</div> " +
-                "</div>");
-        } else if (arr[i].category == "Доходы"){
-            $('.history-flex-container').append("" +
+            break;
+        case "Долги":
+            if(element.price < 0){
+                $('.history-flex-container:last-child').append("" +
+                    "<div class='history-flex-item own-dept' data-toggle='modal' data-target='#history-own-dept__modal-window'>" +
+                    printContentElement(element) +
+                    "</div>");
+            } else {
+                $('.history-flex-container:last-child').append("" +
+                    "<div class='history-flex-item dept' data-toggle='modal' data-target='#history-dept__modal-window'>" +
+                    printContentElement(element) +
+                    "</div>");
+            }
+            break;
+        case "Доходы":
+            $('.history-flex-container:last-child').append("" +
                 "<div class='history-flex-item earnings' data-toggle='modal' data-target='#history-earnings__modal-window'>" +
-                "<div>" +
-                "<strong>"+ arr[i].name +"</strong><br>" +
-                "<small>"+ arr[i].way + "</small> " +
-                "</div> " +
-                "<div> " +
-                "<small>"+ arr[i].category +"</small> " +
-                "<br> " +
-                "<strong>"+ arr[i].price +" &#8381;</strong>" +
-                "</div> " +
+                printContentElement(element) +
                 "</div>");
-        } else {
-            $('.history-flex-container').append("" +
+            break;
+        default:
+            $('.history-flex-container:last-child').append("" +
                 "<div class='history-flex-item transfer' data-toggle='modal' data-target='#history-transfer__modal-window'>" +
-                "<div>" +
-                "<strong>"+ arr[i].name +"</strong><br>" +
-                "<small>"+ arr[i].way + "</small> " +
-                "</div> " +
-                "<div> " +
-                "<small>"+ arr[i].category +"</small> " +
-                "<br> " +
-                "<strong>"+ arr[i].price +" &#8381;</strong>" +
-                "</div> " +
+                printContentElement(element) +
                 "</div>");
+            break;
+    }
+}
+
+function printContentElement(content) {
+    return "<div>" +
+        "<strong>"+ content.name +"</strong><br>" +
+        "<small>"+ content.way + "</small> " +
+        "</div> " +
+        "<div> " +
+        "<small>"+ content.category +"</small> " +
+        "<br> " +
+        "<strong>"+ content.price +" &#8381;</strong>" +
+        "</div> "
+}
+
+function printDate(date) {
+    if ((new Date().getDate() == date.getDate())&&(new Date().getMonth() == date.getMonth())&&(new Date().getFullYear() == date.getFullYear())){
+        $('.history-page').append("<h4>Сегодня</h4>");
+    } else {
+        if (new Date().getDate() - date.getDate() == 1){
+            $('.history-page').append("<h4>Вчера</h4>");
+        } else {
+            $('.history-page').append("<h4>" + date.getDate() + "." + date.getMonth() + "." + date.getFullYear() + "</h4>");
+        }
+    }
+    $('.history-page').append("<div class='history-flex-container'></div>");
+}
+
+function sortDate(arr) {
+    for (var i = 0; i < arr.length - 1; i++){
+        for (var j = 0; j < arr.length - 1 - i; j++){
+            if (arr[j+1].date > arr[j].date){
+                var t = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = t;
+            }
         }
     }
 }
+
